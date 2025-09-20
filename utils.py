7f7b01a8165b67fafa0116605456063d98f24b8c
marloneds-web <marlon.eds@gmail.com>
2025-09-20 07:07:00 -0400

    Atualizar o utils.py

--- utils.py
+++ utils.py
@@ -1,20 +1,17 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
-# ====== Config (ENV) ======
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")  # pode ficar vazio e usar /setchat
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
 POLL_INTERVAL_MINUTES = int(os.getenv("POLL_INTERVAL_MINUTES", "30"))
 MAX_ALERTS_PER_CYCLE = int(os.getenv("MAX_ALERTS_PER_CYCLE", "20"))
-MCAP_MIN = int(os.getenv("MCAP_MIN", "10000000"))        # 10M
-MCAP_MAX = int(os.getenv("MCAP_MAX", "100000000"))       # 100M
-MIN_VOL_24H = float(os.getenv("MIN_VOL_24H", "500000"))  # USD
-MIN_CHANGE_7D = float(os.getenv("MIN_CHANGE_7D", "20"))  # %
-MIN_CHANGE_24H = float(os.getenv("MIN_CHANGE_24H", "5")) # %
+MCAP_MIN = int(os.getenv("MCAP_MIN", "10000000"))
+MCAP_MAX = int(os.getenv("MCAP_MAX", "100000000"))
+MIN_VOL_24H = float(os.getenv("MIN_VOL_24H", "500000"))
+MIN_CHANGE_7D = float(os.getenv("MIN_CHANGE_7D", "20"))
+MIN_CHANGE_24H = float(os.getenv("MIN_CHANGE_24H", "5"))
 
-# ====== CoinGecko ======
 COINGECKO_BASE = "https://api.coingecko.com/api/v3"
 COINS_MARKETS_ENDPOINT = (
     f"{COINGECKO_BASE}/coins/markets?vs_currency=usd&order=market_cap_asc"
